65baac31c2c2fad905c6afbab6138c63e781b422
Wentao Liu <48872641+lseventeen@users.noreply.github.com>
2023-10-13 10:14:08 +0800

    Update fr_unet to old vervion

--- models/fr_unet.py
+++ models/fr_unet.py
@@ -15,15 +15,12 @@ class conv(nn.Module):
             nn.LeakyReLU(0.1, inplace=True),
             nn.Conv2d(out_c, out_c, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_c),
-            nn.Dropout2d(dp))
-        self.relu = nn.LeakyReLU(0.1, inplace=True)
+            nn.Dropout2d(dp),
+            nn.LeakyReLU(0.1, inplace=True))
+        
 
     def forward(self, x):
-        res = x
-        x = self.conv(x)
-        out = x + res
-        out = self.relu(out)
-        return out
+        return self.conv(x)
 
 
 class feature_fuse(nn.Module):
